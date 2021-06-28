e8a7e1b525ebeb1bb3012973c0262157f8c950ab
renee-ross <51426897+renee-ross@users.noreply.github.com>
2021-06-29 00:42:05 +1200

    added tabs to show idea list

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import PySimpleGUI as main
 f = open("ideas.txt", "r")
 Content = f.read()
 
+home = [[main.Text("This is the default")]]
+
 ideas = [
-    [main.Text("Ideas: ")],
+    [main.Text("My Ideas: ")],
     [main.Text(Content)]
 ]
 
 layout = [
     [main.Text("Welcome to my Everything")], 
-    [main.Column(ideas)],
-    [main.Button("OK")]]
+    [main.TabGroup([[main.Tab('Home', home), main.Tab('IDEAS', ideas)]])],
+    [main.Button("END")]]
 
 # Create the window
 w, h = main.Window.get_screen_size()
@@ -24,7 +26,7 @@ while True:
     event, values = window.read()
     # End program if user closes window or
     # presses the OK button
-    if event == "OK" or event == main.WIN_CLOSED:
+    if event == "END" or event == main.WIN_CLOSED:
         break
 
 window.close()
